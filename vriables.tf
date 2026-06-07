1512676eb885f0a3fda58d099723f8ca171034ec
Aniket Siraskar <aniketsiraskar9@gmail.com>
2026-06-07 13:17:06 +0530

    Add variables for AWS configuration

---
+++ vriables.tf
@@ -0,0 +1,15 @@
+variable "aws_region" {}
+variable "ami_id" {}
+variable "subnet_id" {}
+variable "key_name" {}
+
+# Dev
+
+variable "dev_instance_type" {}
+variable "dev_instance_count" {}
+
+variable "test_instance_type" {}
+variable "test_instance_count" {}
+
+variable "prod_instance_type" {}
+variable "prod_instance_count" {}
